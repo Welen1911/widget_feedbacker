04d2109d36ae2873a97fe9f13fa36eae844857af
Welen1911 <welenalmeida2210@gmail.com>
2024-08-03 20:44:26 -0300

    Feito: importacao dos assets utilizados na dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+import palette from './palette';
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: palette
+    },
+    fontFamily: {
+      regular: ["RobotoRegular"],
+      medium: ["RobotoMedium"],
+      bold: ["RobotoBold"],
+      black: ["RobotBlack"],
+    },
   },
   plugins: [],
 };
